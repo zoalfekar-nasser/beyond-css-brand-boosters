cbc9cb5e746ac15807cf291ae523a726a3cac27f
Zoalfekar Nasser <88670561+zoalfekar-nasser@users.noreply.github.com>
2025-08-17 23:32:17 +0300

    Initial commit

---
+++ postcss.config.js
@@ -0,0 +1,10 @@
+import postcssPresetEnv from "postcss-preset-env"; // Use 'import'
+
+export default {
+  // Use 'export default'
+  plugins: [
+    postcssPresetEnv({
+      stage: 3,
+    }),
+  ],
+};
